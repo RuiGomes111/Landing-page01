67a7773322796f5ad2dd520c79e2ca77117a68a6
Rui Gomes <ruisoft01@gmail.com>
2025-06-24 23:53:19 +0100

    reajuste de imagens

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,12 +7,12 @@ const menu = document.querySelector("header")
 modo.addEventListener("click",()=>{   
         if(body.classList.toggle("dark")){
                 
-                img.src="/assets/fotos/sun-moon.svg"
+                img.src="./assets/fotos/sun-moon.svg"
                 img.style.float="left"
                 menu.style.backgroundColor="#010409"
         }else{
                 img.style.float="right"
-                img.src="/assets/fotos/moon.svg"
+                img.src="./assets/fotos/moon.svg"
                 menu.style.backgroundColor="#0D1117"
         }
         
